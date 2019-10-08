eebb37b02a651e1a53931a6cabd6804627c51660
Yash Gupta <yg123yash@gmail.com>
2019-10-08 15:08:44 +0530

    remove console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,3 @@
-console.log("client side js file is loaded");
-
-// fetch("http://puzzle.mead.io/puzzle").then(response => {
-//   response.json().then(data => {
-//     console.log(data);
-//   });
-// });
-
 const weatherForm = document.querySelector("form");
 const searchValue = document.querySelector("input");
 const p1 = document.querySelector("#p1");
